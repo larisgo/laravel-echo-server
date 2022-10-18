4edee0239853b40cec89eddf2657b4e75ff491f2
Luoyy <zishang520@gmail.com>
2022-10-18 14:50:23 +0800

    Update version.

--- utils/version.go
+++ utils/version.go
@@ -1,6 +1,6 @@
 package utils
 
 const (
-	VERSION = "version: v1.1.0"
+	VERSION = "version: v1.1.4"
 	TITLE   = "                         __             __\n|   _  __ _     _  |    |_  _ |_  _    (_  _  __    _  __\n|__(_| | (_|\\_/(/_ |    |__(_ | |(_)   __)(/_ | \\_/(/_ |\n"
 )
